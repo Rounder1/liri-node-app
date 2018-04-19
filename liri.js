312a70d6916e7fafceed361cf9f03a00d636a238
Rounder1 <tolerdavid@gmail.com>
2018-04-19 18:00:55 -0400

    over half done

---
+++ liri.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var request = require("request");
+
+var Spotify1 = require("node-spotify-api");
+var Twitter = require('twitter');
+var spotify = new Spotify1(keys.spotify);
+var client = new Twitter(keys.twitter);
+// use require for the OMDB api, in the ajax giffy homework you can find the key
+var inputString = process.argv;
+
+
+if (inputString[2] === "my-tweets") {    
+    
+    var params = {screen_name: 'thisisntatest1', count: 20};
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+        if(error) {
+            console.log(error);
+        } else {
+
+            for (var i = 0; i < tweets.length; i++) {
+                var tweetNum = 1 + i;
+                console.log("------ Tweet #: " + tweetNum + "---------");
+                console.log(tweets[i].text);
+            }
+        }
+     });
+}
+
+
+if (inputString[2] === "spotify-this-song") {
+
+     var song = "";
+
+    for (var i = 3; i < inputString.length; i++) {
+        song = song + " " + inputString[i];
+    } 
+
+    if (song === "") {
+
+        spotify.search({ type: 'track', query: "The Sign"}, function(err, data) { 
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return; 
+            }
+                //console.log(JSON.stringify(data.tracks.items[5].artists[0].name, null, 2));
+
+                var songAce = data.tracks.items[5];
+
+                console.log(songAce.artists[0].name);
+                console.log(songAce.name);
+                console.log(songAce.preview_url);
+                console.log(songAce.album.name);
+        });  
+        
+    } else {
+
+        spotify.search({ type: 'track', query: song}, function(err, data) { 
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return; 
+            }
+                console.log(song);
+            
+                // console.log(JSON.stringify(data.tracks.items[0], null, 2));
+                var songInfo = data.tracks.items[0];
+
+                console.log(songInfo.artists[0].name);
+                console.log(songInfo.name);
+                console.log(songInfo.preview_url);
+                console.log(songInfo.album.name);
+        });  
+    }  
+}
+
+
+if (inputString[2] === "movie-this") {
+
+}
+
+
+/*
+if (inputString[2] === "do-what-it-says") {
+
+}
+*/
+   
